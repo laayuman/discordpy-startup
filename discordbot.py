b534597d9fdf4cfdd3ab106d99612a7498d1171f
laayuman <70020721+laayuman@users.noreply.github.com>
2020-08-23 13:18:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,17 +16,6 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-    
-
-@client.event
-async def on_ready():
-    message.channnel.send('ﾎﾟｯﾎﾟ！デンショバトが巣に帰って来たよ！')
-
-@client.event
-async def on_message(message):
-	if message.content.isdecimal():
-			text = ("$natural " + message.content + "m send 時間になりました、お疲れさまでした！ to " + message.author.mention)
-			await message.channel.send("リマインダーを設定しました\n" + text)
-
+ 
 
 bot.run(token)
